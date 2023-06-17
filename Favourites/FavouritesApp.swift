26ac39f75ea96d0308e79b4e55876373bcaec6b5
Vladimir <motiw@icloud.com>
2023-06-17 11:01:36 +0600

    Initial Commit

---
+++ Favourites/FavouritesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritesApp.swift
+//  Favourites
+//
+//  Created by Vladimir on 17.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouritesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
